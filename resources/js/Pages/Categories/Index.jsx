a06e73d3f23c5ce3cb446a0b52290d3346e104d3
Moises <amoiseslinares@gmail.com>
2023-08-24 23:34:44 -0500

    initial commit

---
+++ resources/js/Pages/Categories/Index.jsx
@@ -0,0 +1,228 @@
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+export default function Index({ auth, categories }) {
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+          Categories
+        </h2>
+      }
+    >
+      <Head title="Categories" />
+      <div className="py-12">
+        {/* Table Section */}
+        <div className="max-w-[60rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+          {/* Card */}
+          <div className="flex flex-col">
+            <div className="-m-1.5 overflow-x-auto">
+              <div className="p-1.5 min-w-full inline-block align-middle">
+                <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-slate-900 dark:border-gray-700">
+                  {/* Header */}
+                  <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-b border-gray-200 dark:border-gray-700">
+                    {/* Input */}
+                    <div className="sm:col-span-1">
+                      <label
+                        htmlFor="hs-as-table-product-review-search"
+                        className="sr-only"
+                      >
+                        Search
+                      </label>
+                      <div className="relative">
+                        <input
+                          type="text"
+                          id="hs-as-table-product-review-search"
+                          name="hs-as-table-product-review-search"
+                          className="py-2 px-3 pl-11 block w-full border-gray-200 shadow-sm rounded-md text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+                          placeholder="Search"
+                        />
+                        <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none pl-4">
+                          <svg
+                            className="h-4 w-4 text-gray-400"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={16}
+                            height={16}
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                          </svg>
+                        </div>
+                      </div>
+                    </div>
+                    {/* End Input */}
+                    <div className="sm:col-span-2 md:grow">
+                      <div className="flex justify-end gap-x-2">
+                        <Link
+                          className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+                          href={route("categories.create")}
+                        >
+                          Create
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  {/* End Header */}
+                  {/* Table */}
+                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <thead className="bg-gray-50 dark:bg-slate-800">
+                      <tr>
+                        <th scope="col" className="px-6 py-3 text-left">
+                          <div className="flex items-center gap-x-2">
+                            <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                              Nombre
+                            </span>
+                          </div>
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left">
+                          <div className="flex items-center gap-x-2">
+                            <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                              Descripcion
+                            </span>
+                          </div>
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left">
+                          <div className="flex items-center gap-x-2">
+                            <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                              Estado
+                            </span>
+                          </div>
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left">
+                          <div className="flex items-center gap-x-2">
+                            <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                              Date
+                            </span>
+                          </div>
+                        </th>
+                        <th scope="col" className="px-6 py-3 text-left">
+                          <div className="flex items-center gap-x-2">
+                            <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-gray-200">
+                              Acciones
+                            </span>
+                          </div>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                      {categories.data.map((category) => (
+                        <tr
+                          key={category.id}
+                          className="bg-white hover:bg-gray-50 dark:bg-slate-900 dark:hover:bg-slate-800"
+                        >
+                          <td className="h-px w-px whitespace-nowrap">
+                            <a className="block h-full p-6" href="#">
+                              <div className="flex items-center gap-x-4">
+                                <img
+                                  className="flex-shrink-0 h-[2.375rem] w-[2.375rem] rounded-md"
+                                  src="https://images.unsplash.com/photo-1572307480813-ceb0e59d8325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=320&q=80"
+                                  alt="Image Description"
+                                />
+                                <div>
+                                  <span className="block text-sm font-semibold text-gray-800 dark:text-gray-200">
+                                    {category.name}
+                                  </span>
+                                </div>
+                              </div>
+                            </a>
+                          </td>
+                          <td className="h-px whitespace-nowrap">
+                            <a className="block h-full p-6" href="#">
+                              <div className="flex items-center gap-x-3">
+                                {category.description.slice(0, 15)}
+                              </div>
+                            </a>
+                          </td>
+                          <td className="h-px">
+                            <a className="block h-full p-6" href="#">
+                              <span className="inline-flex items-center gap-1.5 py-0.5 px-2 rounded-full text-xs font-medium bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200">
+                                <svg
+                                  className="w-2.5 h-2.5"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width={16}
+                                  height={16}
+                                  fill="currentColor"
+                                  viewBox="0 0 16 16"
+                                >
+                                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                </svg>
+                                {category.state}
+                              </span>
+                            </a>
+                          </td>
+                          <td className="h-px whitespace-nowrap">
+                            <a className="block h-full p-6" href="#">
+                              <span className="text-sm text-gray-600 dark:text-gray-400">
+                                10 Jan 2022
+                              </span>
+                            </a>
+                          </td>
+                          <td className="h-px whitespace-nowrap"></td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  {/* End Table */}
+                  {/* Footer */}
+                  <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-gray-700">
+                    <div className="max-w-sm space-y-3">
+                      <select className="py-2 px-3 pr-9 block w-full border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400">
+                        <option value="1">1</option>
+                      </select>
+                    </div>
+                    <div>
+                      <div className="inline-flex gap-x-2">
+                        <button
+                          type="button"
+                          className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+                        >
+                          <svg
+                            className="w-3 h-3"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={16}
+                            height={16}
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                            />
+                          </svg>
+                          Prev
+                        </button>
+                        <button
+                          type="button"
+                          className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+                        >
+                          Next
+                          <svg
+                            className="w-3 h-3"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={16}
+                            height={16}
+                            fill="currentColor"
+                            viewBox="0 0 16 16"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  {/* End Footer */}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* End Card */}
+        </div>
+        {/* End Table Section */}
+      </div>
+    </AuthenticatedLayout>
+  );
+}
